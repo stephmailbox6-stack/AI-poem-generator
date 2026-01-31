324a68190e26c7aa988c829f0bec2a174c5bf090
stephmailbox6-stack <stephmailbox6@gmail.com>
2026-01-31 09:53:16 +0000

    added form functionality and typewriter effect

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
+function myPoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "The poem will go here",
+    autoStart: true,
+    delay: 40,
+    cursor: "",
+  });
+}
+
+let newPoemElement = document.querySelector("#poem-generator-form");
+newPoemElement.addEventListener("submit", myPoem);
